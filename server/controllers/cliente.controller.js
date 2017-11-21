407f3fd59dfaf036d5e34b3da2be9b06b2bc13f8
danizavtz <daniel.pires@splab.ufcg.edu.br>
2017-11-21 11:19:21 -0300

    Nova sintaxe usando async e await

--- server/controllers/cliente.controller.js
+++ server/controllers/cliente.controller.js
@@ -3,19 +3,19 @@
   const db = require('../../mysql/db');
 
 
-  exports.lookupCliente = (req, res, next) => {
+  exports.lookupCliente = async(req, res, next) => {
   	let sql = 'SELECT c.* FROM cliente c WHERE c.id = $1';
-    db.client.query(sql,[req.params.id], (err, result) => {
-	    if (err) {
-	        return res.status(500).json({ errors: ['Could not query cliente'] });
-	    }
-	    if (result.rows.length === 0) {
+    try{
+      const result = await db.client.query(sql, [req.params.id]);
+      if (result.rows.length === 0) {
           return res.status(404).json({ errors: [{ param: 'id', msg: 'Not found', value: req.params.id }] });
-	    }
-	    req.cliente = result.rows[0];
-	    return next();
-	 });
-  }
+      }
+      req.cliente = result.rows[0];
+      next();
+    } catch (e) {
+      return res.status(500).json({ errors: [{ param: "", msg: e.detail, value: "" }] });
+    }
+	 };
 
   exports.getCliente = (req, res) => {
   	res.status(200).json(req.cliente);
@@ -29,41 +29,41 @@
       res.status(200).json(results.rows);
       res.end();
     } catch (e) {
-      return res.status(500).json({ errors: [{ param: undefined, msg: 'Could not query cliente', value: undefined }] });
+      return res.status(500).json({ errors: [{ param: "", msg: e.detail, value: "" }] });
     }    
   };
 
-  exports.addCliente = (req, res) => {
+  exports.addCliente = async(req, res) => {
   	let sql = 'INSERT INTO cliente(login,email,phone) VALUES ($1,$2,$3) RETURNING *';
-  	db.client.query(sql,[req.body.login || null, req.body.email, req.body.phone], (err, result) => {
-  		if(err){
-  			return res.status(500).json({ errors: [{ param: 'id', msg: 'Could not query cliente', value: req.params.id }] });
-  		}
-  		res.status(201).json(result.rows[0]);
-  		res.end();
-  	});
+    try {
+      const result = await db.client.query(sql,[req.body.login || null, req.body.email, req.body.phone]);
+      res.status(201).json(result.rows[0]);
+      res.end();
+    } catch (e) {
+      return res.status(500).json({ errors: [{ param: 'id', msg: e.detail, value: ""}] });
+    }
   };
 
-  exports.updateCliente = (req, res) => {
+  exports.updateCliente = async(req, res) => {
   	let sql = 'UPDATE cliente SET login = $1, email = $2, phone = $3 WHERE id = $4 RETURNING *';
-  	db.client.query(sql, [req.body.login || req.cliente.login, req.body.email, req.body.phone, req.params.id], (err, result) =>{
-  		if(err){
-  			return res.status(500).json({ errors: [{ param: 'id', msg: 'Could not query cliente', value: req.params.id }] });
-  		}
-  		res.status(200).json(result.rows[0]);
-  		res.end();
-  	})
+    try{
+      const result = await db.client.query(sql, [req.body.login || req.cliente.login, req.body.email, req.body.phone, req.params.id]);  	
+      res.status(200).json(result.rows[0]);
+      res.end();
+    } catch (e){
+      return res.status(500).json({ errors: [{ param: "", msg: e.detail, value: ""}] });      
+    }
   };
 
-  exports.deleteCliente = (req, res) => {
+  exports.deleteCliente = async(req, res) => {
   	let sql = 'DELETE FROM cliente WHERE id = $1';
-  	db.client.query(sql, [req.params.id], (err, result) =>{
-  		if(err){ 
-  			return res.status(500).json({ errors: [{ param: 'id', msg: 'Could not query cliente', value: req.params.id }] });
-  		}
-  		res.status(204);
-  		res.end();
-  	});
+    try {
+      const result = await db.client.query(sql, [req.params.id]);
+      res.status(204);
+      res.end();
+    } catch (e) {
+      return res.status(500).json({ errors: [{ param: "", msg: e.detail, value: "" }] });
+    }
   };
 
 })();
